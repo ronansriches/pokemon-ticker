9fec59e5c62fad7fbdb1c271ae7e38391f9a24b6
ronansriches <ronansriches@gmail.com>
2025-10-27 13:31:19 -0400

    Update movers.js

--- api/movers.js
+++ api/movers.js
@@ -1,9 +1,13 @@
 export default async function handler(req, res) {
   try {
-    const url = "https://api.justtcg.com/v1/market-movers?game=pokemon&limit=20";
-    const r = await fetch(url, {
-      headers: { "x-api-key": process.env.JUSTTCG_API_KEY }
-    });
+    const url = "https://api.justtcg.com/v1/cards"
+      + "?game=pokemon"
+      + "&orderBy=30d"                 // sort by 30-day % change
+      + "&limit=20"
+      + "&include_price_history=true"
+      + "&include_statistics=7d,30d";
+
+    const r = await fetch(url, { headers: { "x-api-key": process.env.JUSTTCG_API_KEY } });
     const text = await r.text();
     res.status(r.status).send(text);
   } catch (err) {
